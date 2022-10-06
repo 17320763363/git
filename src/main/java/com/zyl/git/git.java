5c580778a7b38154d9aba1b2ab5f537c2557e0ef
17320763363 <>
2022-10-06 18:09:43 +0800

    second hot-fix

--- src/main/java/com/zyl/git/git.java
+++ src/main/java/com/zyl/git/git.java
@@ -6,6 +6,8 @@ public class git {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot-fxt test");
+        System.out.println("hot-fxt test2");
 
     }
 }
